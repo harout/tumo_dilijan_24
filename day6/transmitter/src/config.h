e00316248a284974b445471d78ee522477263fb6
harout <harout@harouts-MacBook-Air.local>
2024-02-12 04:09:52 -0800

    Adding code for day 6

---
+++ day6/transmitter/src/config.h
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+#define BUILD_FOR                   "heltec"
+#define OLED_SDA                    17
+#define OLED_SCL                    18
+#define OLED_RST                    21
+
+#define RADIO_SCLK_PIN              9
+#define RADIO_MISO_PIN              11
+#define RADIO_MOSI_PIN              10
+#define RADIO_CS_PIN                8
+#define RADIO_DIO1_PIN              14
+#define RADIO_RST_PIN               12
+#define RADIO_BUSY_PIN              13
+
+#define DEFAULT_FREQUENCY			912
+#define MAX_RADIO_OUTPUT_POWER		22
+#define MIN_SPREADING_FACTOR		5
+#define MAX_SPREADING_FACTOR		12
+#define DEFAULT_SPREADING_FACTOR    6
+#define MAX_CODING_RATE				8
+#define DEFAULT_CODING_RATE         8
+#define DEFAULT_BANDWIDTH			500
+#define MAX_BANDWIDTH				500
+
+#define BOARD_LED                   35
+#define LED_ON                      HIGH
+
+#define BUTTON_PIN                  0
+
---
+++ day6/transmitter/src/main.cpp
@@ -0,0 +1,178 @@
+#include <Arduino.h>
+#include <RadioLib.h>
+#include <Wire.h>
+#include <Ticker.h>
+#include <U8g2lib.h>
+
+#include "config.h"
+
+#define PACKET_LENGTH 32
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C *u8g2 = nullptr;
+Ticker ledTicker;
+
+SX1262 radio = NULL;
+
+// flag to indicate that a packet was sent or received
+volatile bool operationDone = false;
+
+
+void debugOutput(String lineOne,
+                 String lineTwo = "",
+                 String lineThree = "",
+                 String lineFour = "",
+                 String lineFive = "",
+                 String lineSix = "")
+{
+  if (u8g2)
+  {
+    u8g2->setFont(u8g2_font_6x10_tf);
+    u8g2->clearBuffer();
+    u8g2->drawStr(0, 10, lineOne.c_str());
+    u8g2->drawStr(0, 20, lineTwo.c_str());
+    u8g2->drawStr(0, 30, lineThree.c_str());
+    u8g2->drawStr(0, 40, lineFour.c_str());
+    u8g2->drawStr(0, 50, lineFive.c_str());
+    u8g2->drawStr(0, 60, lineSix.c_str());
+    u8g2->sendBuffer();
+  }
+}
+
+// this function is called when a complete packet
+// is transmitted or received by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+#if defined(ESP8266) || defined(ESP32)
+  ICACHE_RAM_ATTR
+#endif
+void setFlag(void) {
+  // we sent or received  packet, set the flag
+  operationDone = true;
+}
+
+
+
+void setupRadio()
+{
+  radio = new Module(RADIO_CS_PIN,
+                     RADIO_DIO1_PIN,
+                     RADIO_RST_PIN,
+                     RADIO_BUSY_PIN);
+
+  
+  int state = radio.begin(DEFAULT_FREQUENCY,
+                          DEFAULT_BANDWIDTH,
+                          DEFAULT_SPREADING_FACTOR,
+                          DEFAULT_CODING_RATE);
+  if (state != RADIOLIB_ERR_NONE)
+  {
+    debugOutput("Failed!", "Error at radio.begin");
+    while (true)
+      ;
+  }
+
+  radio.implicitHeader(PACKET_LENGTH);
+
+  if (radio.setOutputPower(MAX_RADIO_OUTPUT_POWER) != RADIOLIB_ERR_NONE)
+  {
+    debugOutput("Failed!", "Invalid power level.");
+    while (true)
+      ;
+  }
+
+  // set the function that will be called
+  // when packet transmission is finished
+  radio.setDio1Action(setFlag);
+}
+
+
+void initBoard()
+{
+    SPI.begin(RADIO_SCLK_PIN, RADIO_MISO_PIN, RADIO_MOSI_PIN);
+    Wire.begin(OLED_SDA, OLED_SCL);
+    
+    Serial.begin(115200);
+    Serial1.begin(115200, SERIAL_8N1, 47, 48);
+
+    Serial1.println("Initializing board.");
+    //delay(2000);
+
+    pinMode(BOARD_LED, OUTPUT);
+    ledTicker.attach_ms(350, []() {
+        static bool level;
+        digitalWrite(BOARD_LED, level);
+        level = !level;
+    });
+    Serial1.println("Set up ticker.");
+
+    #if OLED_RST
+    pinMode(OLED_RST, OUTPUT);
+    
+    digitalWrite(OLED_RST, HIGH);
+    delay(50);
+
+    digitalWrite(OLED_RST, LOW);
+    delay(200);
+
+    digitalWrite(OLED_RST, HIGH);
+    delay(50);    
+    #endif
+
+    Serial1.println("Reset OLED.");
+    //delay(1000);
+
+    Wire.beginTransmission(0x3C);
+    if (Wire.endTransmission() == 0) {
+        u8g2 = new U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0, OLED_RST);
+        u8g2->begin();
+        u8g2->clearBuffer();
+        u8g2->setFlipMode(0);
+        u8g2->setFontMode(1); // Transparent
+        u8g2->setDrawColor(1);
+        u8g2->setFontDirection(0);
+        u8g2->setFont(u8g2_font_t0_11_t_all);
+
+        u8g2->drawStr(0, 10, "TUMO");
+        u8g2->drawStr(0, 30, "Transmitter");
+        u8g2->drawUTF8(0, 50, "ԱԲԳ");
+        u8g2->sendBuffer();
+    }
+}
+
+
+void setup()
+{
+  initBoard();
+  delay(1500);
+
+  debugOutput("Starting up.", "Will bring up radio.");
+  delay(1000);
+
+  setupRadio();
+}
+
+
+void txMessage(uint8_t command){
+  uint8_t b[PACKET_LENGTH];
+
+  // write code to fill the packet buffer with the
+  // command byte
+
+  int status = radio.transmit(b, PACKET_LENGTH);
+}
+
+
+void loop()
+{
+  // write code to send a clear screen command
+
+  delay(1000);
+
+  // write code to send a checkers command
+
+  delay(1000);
+
+  // write code to send a fill screen command
+
+  delay(1000);
+}
